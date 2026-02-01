a2d0763a9483d177eb8fc8916c67328d28199cd7
Imagine-Choi <sang8408@naver.com>
2026-02-01 14:37:46 +0900

    chore/#9: BuyOrNotImgs.kt 파일 KDoc 변경

--- core/designsystem/src/main/java/com/sseotdabwa/buyornot/core/designsystem/icon/BuyOrNotImgs.kt
+++ core/designsystem/src/main/java/com/sseotdabwa/buyornot/core/designsystem/icon/BuyOrNotImgs.kt
@@ -1,41 +1,39 @@
 package com.sseotdabwa.buyornot.core.designsystem.icon
 
 import androidx.annotation.DrawableRes
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
 import com.sseotdabwa.buyornot.core.designsystem.R
 
 /**
- * BuyOrNot 프로젝트 전체에서 사용하는 이미지 모음
- * Drawable 리소스의 커스텀 png, jpg 등의 이미지들을 통합 관리
+ * BuyOrNot 프로젝트 전체에서 사용하는 비트맵 이미지 모음
  *
- */
-object BuyOrNotImgs {
-    //로그인 화면 배경
-    val img_login_bg = IconResource(R.drawable.img_login_bg)
-}
-
-/**
- * Drawable 리소스를 래핑하는 타입
- * SVG 아이콘을 타입 안전하게 관리하기 위해 사용
+ * PNG, JPG 등의 비트맵 이미지 리소스를 통합 관리합니다.
+ * 벡터 아이콘(SVG)은 [BuyOrNotIcons]를 사용하세요.
  *
  * 사용 예시:
  * ```
- * Icon(
- *     imageVector = BuyOrNotIcons.Vote.asImageVector(),
- *     contentDescription = "투표"
+ * Image(
+ *     painter = painterResource(id = BuyOrNotImgs.LoginBackground.resId),
+ *     contentDescription = "로그인 배경"
  * )
  * ```
+ */
+object BuyOrNotImgs {
+    /**
+     * 로그인 화면 배경 이미지
+     */
+    val LoginBackground = ImgsResource(R.drawable.img_login_bg)
+}
+
+/**
+ * Drawable 비트맵 리소스를 래핑하는 타입
+ *
+ * PNG, JPG 등의 비트맵 이미지를 타입 안전하게 관리하기 위해 사용합니다.
+ * painterResource()와 함께 사용하여 Compose에서 이미지를 표시합니다.
+ *
+ * @property resId drawable 리소스 ID
  */
 @JvmInline
 value class ImgsResource(
     @param:DrawableRes val resId: Int,
 )
 
-/**
- * ImgsResource를 ImageVector로 변환하는 확장 함수
- * Composable 컨텍스트에서만 사용 가능
- */
-@Composable
-fun ImgsResource.asImageVector(): ImageVector = ImageVector.vectorResource(id = resId)
