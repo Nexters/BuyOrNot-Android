6b59c0fef6b8d1a3f927473a3f4bf1b8d8fbe9ae
dongchyeon <kimdonghyun90766@gmail.com>
2026-02-02 20:48:38 +0900

    refactor/#13: rotate() 대신 graphicsLayer를 이용해 회전 애니메이션 구현

--- core/designsystem/src/main/java/com/sseotdabwa/buyornot/core/designsystem/components/ExpandableFab.kt
+++ core/designsystem/src/main/java/com/sseotdabwa/buyornot/core/designsystem/components/ExpandableFab.kt
@@ -28,8 +28,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -93,7 +93,9 @@ fun ExpandableFloatingActionButton(
                 modifier =
                     Modifier
                         .size(24.dp)
-                        .rotate(rotationAngle),
+                        .graphicsLayer {
+                            rotationZ = rotationAngle
+                        },
             )
         }
     }
