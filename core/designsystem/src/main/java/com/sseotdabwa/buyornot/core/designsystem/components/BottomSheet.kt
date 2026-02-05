f2c84204533ca00192f1a7f66370e664f39cef81
dongchyeon <kimdonghyun90766@gmail.com>
2026-02-06 01:54:29 +0900

    feat/#19: BottomSheet 닫을 때 targetValue 체크를 통해 중복 실행 방지

--- core/designsystem/src/main/java/com/sseotdabwa/buyornot/core/designsystem/components/BottomSheet.kt
+++ core/designsystem/src/main/java/com/sseotdabwa/buyornot/core/designsystem/components/BottomSheet.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -71,9 +72,11 @@ fun BuyOrNotBottomSheet(
     val screenHeight = with(LocalDensity.current) { windowInfo.containerSize.height.toDp() }
 
     val hideSheetWithAnimation: () -> Unit = {
-        scope.launch { sheetState.hide() }.invokeOnCompletion {
-            if (!sheetState.isVisible) {
-                onDismissRequest()
+        if (sheetState.targetValue != SheetValue.Hidden) {
+            scope.launch { sheetState.hide() }.invokeOnCompletion {
+                if (!sheetState.isVisible) {
+                    onDismissRequest()
+                }
             }
         }
     }
