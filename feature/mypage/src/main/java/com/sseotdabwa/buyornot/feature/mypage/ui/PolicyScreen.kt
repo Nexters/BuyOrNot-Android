69480b31b1e5f7eb99e4331b9fdaf40109846bb8
dongchyeon <kimdonghyun90766@gmail.com>
2026-02-10 11:34:58 +0900

    feat/#22: 약관 및 정책 UI 구현

--- feature/mypage/src/main/java/com/sseotdabwa/buyornot/feature/mypage/ui/PolicyScreen.kt
+++ feature/mypage/src/main/java/com/sseotdabwa/buyornot/feature/mypage/ui/PolicyScreen.kt
@@ -1,18 +1,17 @@
 package com.sseotdabwa.buyornot.feature.mypage.ui
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.sseotdabwa.buyornot.core.designsystem.components.BackTopBar
+import androidx.compose.ui.unit.dp
+import com.sseotdabwa.buyornot.core.designsystem.components.BackTopBarWithTitle
 import com.sseotdabwa.buyornot.core.designsystem.theme.BuyOrNotTheme
+import com.sseotdabwa.buyornot.feature.mypage.component.SettingItem
 
 @Composable
 fun PolicyRoute(onBackClick: () -> Unit) {
@@ -25,21 +24,17 @@ fun PolicyScreen(
     onBackClick: () -> Unit,
 ) {
     Column(modifier = modifier.fillMaxSize()) {
-        BackTopBar(onBackClick = onBackClick)
+        BackTopBarWithTitle(
+            title = "약관 및 정책",
+            onBackClick = onBackClick,
+        )
 
         Column(
-            modifier =
-                Modifier
-                    .weight(1f)
-                    .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(top = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center,
-            ) {
-                Text(text = "Policy Screen")
-            }
+            SettingItem("개인정보처리방침") { }
+            SettingItem("서비스 약관") { }
         }
     }
 }
