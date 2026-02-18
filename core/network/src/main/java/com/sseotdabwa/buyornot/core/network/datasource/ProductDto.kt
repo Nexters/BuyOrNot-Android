3599334e5fd55f8decf3a57f2723930aa176a7f6
dongchyeon <kimdonghyun90766@gmail.com>
2026-02-19 00:04:04 +0900

    chore/#38: 불필요한 FakeProductNetworkDataSource 관련 코드 제거

--- core/network/src/main/java/com/sseotdabwa/buyornot/core/network/datasource/ProductDto.kt
+++
@@ -1,9 +0,0 @@
-package com.sseotdabwa.buyornot.core.network.datasource
-
-data class ProductDto(
-    val id: String,
-    val title: String,
-    val description: String,
-    val price: Int,
-    val imageUrl: String,
-)
--- core/network/src/main/java/com/sseotdabwa/buyornot/core/network/datasource/ProductNetworkDataSource.kt
+++
@@ -1,5 +0,0 @@
-package com.sseotdabwa.buyornot.core.network.datasource
-
-interface ProductNetworkDataSource {
-    suspend fun fetchProducts(): List<ProductDto>
-}
