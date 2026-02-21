ae7c2d02a8a289e63562179f89e7ca60a1bfb33c
Imagine-Choi <sang8408@naver.com>
2026-02-21 15:07:00 +0900

    refactor/#42: userPreferencesDataSource만 의존하도록 변경

--- core/data/src/main/java/com/sseotdabwa/buyornot/core/data/repository/AuthRepositoryImpl.kt
+++ core/data/src/main/java/com/sseotdabwa/buyornot/core/data/repository/AuthRepositoryImpl.kt
@@ -1,21 +1,25 @@
 package com.sseotdabwa.buyornot.core.data.repository
 
 import com.sseotdabwa.buyornot.core.datastore.UserPreferencesDataSource
+import com.sseotdabwa.buyornot.core.datastore.UserType
 import com.sseotdabwa.buyornot.core.network.api.AuthApiService
 import com.sseotdabwa.buyornot.core.network.dto.request.GoogleLoginRequest
 import com.sseotdabwa.buyornot.core.network.dto.request.KakaoLoginRequest
 import com.sseotdabwa.buyornot.core.network.dto.request.RefreshRequest
 import com.sseotdabwa.buyornot.core.network.dto.response.getOrThrow
-import com.sseotdabwa.buyornot.domain.model.UserType
 import com.sseotdabwa.buyornot.domain.repository.AuthRepository
-import com.sseotdabwa.buyornot.domain.repository.UserPreferencesRepository
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
+/**
+ * 인증 관련 비즈니스 로직을 처리하는 Repository 구현체
+ *
+ * AuthApiService를 통해 서버와 통신하고,
+ * UserPreferencesDataSource를 통해 토큰과 사용자 타입을 로컬에 저장합니다.
+ */
 class AuthRepositoryImpl @Inject constructor(
     private val authApiService: AuthApiService,
     private val userPreferencesDataSource: UserPreferencesDataSource,
-    private val userPreferencesRepository: UserPreferencesRepository,
 ) : AuthRepository {
     override suspend fun googleLogin(idToken: String) {
         val tokenData = authApiService.googleLogin(GoogleLoginRequest(idToken)).getOrThrow()
@@ -23,7 +27,7 @@ class AuthRepositoryImpl @Inject constructor(
             accessToken = tokenData.accessToken,
             refreshToken = tokenData.refreshToken,
         )
-        userPreferencesRepository.updateUserType(UserType.SOCIAL)
+        userPreferencesDataSource.updateUserType(UserType.SOCIAL)
     }
 
     override suspend fun kakaoLogin(accessToken: String) {
@@ -32,7 +36,7 @@ class AuthRepositoryImpl @Inject constructor(
             accessToken = tokenData.accessToken,
             refreshToken = tokenData.refreshToken,
         )
-        userPreferencesRepository.updateUserType(UserType.SOCIAL)
+        userPreferencesDataSource.updateUserType(UserType.SOCIAL)
     }
 
     override suspend fun logout() {
