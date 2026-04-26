dbf2d0b1359291d84b77b6b5f75345781709d1f6
dongchyeon <kimdonghyun90766@gmail.com>
2026-04-26 13:36:58 +0900

    chore/#29: kotlin.serialization 플러그인 루트 build.gradle.kts에 추가

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -31,7 +31,6 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                 add("implementation", libs.findLibrary("androidx.lifecycle.runtime.compose").get())
                 add("implementation", libs.findLibrary("androidx.navigation.compose").get())
                 add("implementation", libs.findLibrary("hilt.navigation.compose").get())
-
             }
         }
     }
