ec96bd8153d278d9042e117e792c995416388a8e
dongchyeon <kimdonghyun90766@gmail.com>
2026-05-01 15:26:55 +0900

    feat/#100: android-image-cropper 의존성 및 CropImageActivity Manifest 등록

--- feature/upload/build.gradle.kts
+++ feature/upload/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation(projects.core.designsystem)
     implementation(projects.core.ui)
     implementation(libs.coil.compose)
+    implementation(libs.android.image.cropper)
 
     testImplementation(libs.junit)
     testImplementation(libs.kotlin.test)
